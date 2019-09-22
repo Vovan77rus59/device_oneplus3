870aeb3a087ff0acb19a6c861dffc4e24438becd
Pranav Vashi <neobuddy89@gmail.com>
2019-09-22 13:31:47 +0530

    op3: Create system copy of ims-ext-common and add to classpath
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- device.mk
+++ device.mk
@@ -431,10 +431,11 @@ PRODUCT_PACKAGES += \
 
 # Telephony
 PRODUCT_PACKAGES += \
-    ims-ext-common \
+    ims-ext-common_system \
     telephony-ext
 
-#PRODUCT_BOOT_JARS += \
+PRODUCT_BOOT_JARS += \
+    ims-ext-common_system \
     telephony-ext
 
 # TextClassifier smart selection model files
